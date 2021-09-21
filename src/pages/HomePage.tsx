e4e3a5657bbbbe4810285bb09e25867f85551a9d
Jonathan Soukaseume <sksmejn@gmail.com>
2021-09-21 15:26:45 -0700

    Created API endpoint and fetched the quiz data. Created utils to shuffle questions from the given fetched data. Updated HomePage with answerObject type to type out the answers.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,25 @@
 import { QuestionCard } from "../components";
 import { useState } from "react";
+import { fetchData, Difficulty, QuestionState } from "../API";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  coorect: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 10;
 
 export default function HomePage() {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchData(TOTAL_QUESTIONS, Difficulty.EASY));
 
   const startTrivia = async () => {
     console.log("Start Trivia Here");
@@ -31,14 +41,14 @@ export default function HomePage() {
       </button>
       <p className="score">Score: </p>
       <p>Loading...</p>
-      <QuestionCard
+      {/* <QuestionCard
         questionNumber={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswer ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
       <button className="nextButton" onClick={nextQuestion}>
         Next
       </button>
